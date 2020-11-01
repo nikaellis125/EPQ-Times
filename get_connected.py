d0ff58ee1988f75d305f6211fb4664089ebd533a
luisquiles28 <quilesl1@southernct.edu>
2020-10-31 22:01:20 -0400

    lq 2

--- get_connected.py
+++ get_connected.py
@@ -1,8 +1,12 @@
 import pyodbc
 server = 'epqtimeclock.database.windows.net'
 database = 'EPQTimeClockdb'
-password = 'Dap_1223'
-username = 'Divyesh12'   
-driver= '{ODBC Driver 17 for SQL Server}'
+username = 'Dap_1223'
+password = 'Divyesh12'
+driver = '{ODBC Driver 17 for SQL Server}'
+
+
 def get_connected():
-    return pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+password)
+    return pyodbc.connect('DRIVER=' + driver + ';SERVER=' + server +
+                          ';PORT=1433;DATABASE=' + database + ';UID=' +
+                          username + ';PWD=' + password)
